2755b96b93a31854a2f31fff868aa87b78d065aa
terasakisatoshi <terasakisatoshi.math@gmail.com>
2021-01-24 02:43:00 +0000

    Deploying to gh-pages from  @ 31fc84003f833fe3cd914f32e5acd70c401048ab 🚀

--- assets/index/code/welcome.jl
+++
@@ -1,9 +0,0 @@
-# This file was generated, do not modify it. # hide
-#hideall
-using Luxor
-
-@svg begin
-    fontsize(25)
-    fontface("Zapfino")
-    text("Welcome to Satoshi Terasaki's website", halign=:center, valign=:middle)
-end 600 200 joinpath(@OUTPUT, "welcome")
